ec5c42a72620c2f3bd392180f0a79143b94f6a5c
nischal <nischalkshaj5@gmail.com>
2024-09-18 01:52:05 +0530

    image editing and deletion completed

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,16 +4,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { AppState } from "../../store";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const user = AppState((state) => state.user);
   const baseUrl = import.meta.env.VITE_BASE_URL;
+  const navigate = useNavigate();
+  const isAuthorized = AppState((state) => state.isAuthorized);
   const [file, setFile] = useState<File | null>(null);
   const [imageName, setImageName] = useState("");
   const [uploadedFile, setUploadedFile] = useState<
-    { name: string; url: string }[]
+    { title: string; imageUrl: string; _id: string }[]
   >([]);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [editMode, setEditMode] = useState(false);
+  const [editingImageId, setEditingImageId] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!isAuthorized) {
+      navigate("/");
+    }
+  }, [isAuthorized, navigate]);
 
   const fetchData = async () => {
     const token = localStorage.getItem("access_token");
@@ -29,7 +40,9 @@ const Home = () => {
       });
       if (response.status === 202) {
         console.log(response.data);
-        setUploadedFile(response.data.data.images);
+        const imageData = response.data.data.images;
+        console.log("img", imageData);
+        setUploadedFile(imageData);
       }
     } catch (error) {
       console.error("error", error);
@@ -56,8 +69,13 @@ const Home = () => {
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
-    if (!file || !imageName) {
-      alert("select an image or add the image name");
+    if (!file && !editMode) {
+      alert("Please select an image to upload.");
+      return;
+    }
+
+    if (!imageName) {
+      alert("Please enter an image name.");
       return;
     }
 
@@ -65,39 +83,112 @@ const Home = () => {
     const id = user?._id;
 
     const formData = new FormData();
-    formData.append("image", file);
+    if (file) {
+      formData.append("image", file);
+    }
     formData.append("imageName", imageName);
 
     try {
-      const response = await axios.post(`${baseUrl}/uploads/${id}`, formData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        withCredentials: true,
-      });
-      if (response.status === 201) {
-        console.log("image added successfully");
-        alert("image uploaded successfully");
-        setUploadedFile((prevData) => [
-          ...prevData,
+      if (editMode && editingImageId) {
+        // Update existing image
+        const response = await axios.patch(
+          `${baseUrl}/image/edit/${id}/${editingImageId}`,
+          formData,
           {
-            name: response.data.data.image.name,
-            url: response.data.data.image.url,
-          },
-        ]);
-        setFile(null);
-        setImageName("");
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "multipart/form-data",
+            },
+            withCredentials: true,
+          }
+        );
+
+        if (response.status === 200) {
+          const imageData = response.data.data.images;
+          alert("Image updated successfully");
+          setUploadedFile(imageData);
+        }
+      } else {
+        // Add a new image
+        const response = await axios.patch(
+          `${baseUrl}/uploads/${id}`,
+          formData,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "multipart/form-data",
+            },
+            withCredentials: true,
+          }
+        );
+
+        if (response.status === 201) {
+          const imageData = response.data.data.images;
+          alert("Image uploaded successfully");
+          setUploadedFile(imageData);
+        }
+      }
+
+      // Clear input fields after upload/edit
+      setFile(null);
+      setImageName("");
+      setPreviewUrl(null);
+      setEditMode(false);
+      setEditingImageId(null);
+
+      const fileInput = document.querySelector(
+        'input[type="file"]'
+      ) as HTMLInputElement;
+      if (fileInput) {
+        fileInput.value = ""; // Clear file input
       }
     } catch (error) {
       console.error("error", error);
     }
   };
 
+  const handleDelete = async (_id: string) => {
+    const id = user?._id;
+    const token = localStorage.getItem("access_token");
+    try {
+      const response = await axios.delete(`${baseUrl}/image/${id}/${_id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      });
+
+      if (response.status === 202) {
+        const imageData = response.data.data.images;
+        alert("image deleted successfully");
+        setUploadedFile(imageData);
+      }
+    } catch (error) {
+      console.error("error", error);
+    }
+  };
+
+  const handleEdit = (_id: string) => {
+    // Find the image being edited
+    const imageToEdit = uploadedFile.find((file) => file._id === _id);
+
+    if (imageToEdit) {
+      setImageName(imageToEdit.title); // Set image name in input field
+      setPreviewUrl(imageToEdit.imageUrl); // Set preview image
+      setEditMode(true); // Set to edit mode
+      setEditingImageId(_id); // Set the ID of the image being edited
+    }
+  };
+
   return (
-    <div className="bg-custom w-full flex justify-center items-center min-h-screen">
-      <div className="flex flex-col justify-center items-center outline-dotted">
-        <h6>welcome {user?.username}</h6>
-        <p>Upload your Image here</p>
+    <div className="bg-custom w-full flex flex-col justify-center items-center min-h-screen space-y-6 p-4">
+      <div className="bg-white shadow-md rounded-lg p-8 flex flex-col justify-center items-center space-y-4 outline-dotted">
+        <h2 className="text-2xl font-semibold text-gray-700">
+          Welcome, {user?.username}
+        </h2>
+        <p className="text-lg text-gray-600">
+          {editMode ? "Edit Image" : "Upload Image"}
+        </p>
         <input
           type="text"
           name="image_name"
@@ -106,6 +197,7 @@ const Home = () => {
           onChange={handleImageNameChange}
           placeholder="Enter image name"
           required
+          className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <input
           type="file"
@@ -116,31 +208,48 @@ const Home = () => {
           <img
             src={previewUrl}
             alt="Image Preview"
-            className="w-32 h-32 object-cover mt-4"
+            className="w-96 h-96 object-cover mt-4 rounded-lg shadow-lg"
           />
         )}
         <button
-          className="bg-blue-500 text-white"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-2 rounded-lg shadow-lg transition duration-300 ease-in-out"
           type="button"
           onClick={handleSubmit}
         >
-          submit
+          {editMode ? "Update Image" : "Submit"}
         </button>
       </div>
-      <div className="mt-8">
-        <h3>Uploaded Images:</h3>
+
+      <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-lg mt-8">
+        <h3 className="text-xl font-semibold mb-4">Uploaded Images:</h3>
         {uploadedFile.length === 0 ? (
           <p>No images uploaded</p>
         ) : (
-          <ul>
+          <ul className="flex space-x-8 overflow-x-auto">
             {uploadedFile.map((file, index) => (
-              <li key={index}>
-                <p className="text-lg p-2 ">{file.name}</p>
-                <img
-                  src={`${baseUrl}/image/${file.url}`}
-                  alt={file.name}
-                  className="w-32 h-32 object-cover"
-                />
+              <li key={index} className="flex flex-col items-center space-y-2">
+                <div className="text-center">
+                  <p className="text-lg text-gray-700">{file.title}</p>
+                  <img
+                    src={file.imageUrl}
+                    alt={file.title}
+                    className="w-40 h-40 object-cover rounded-md shadow-md"
+                  />
+                  <div className="flex flex-col space-y-2 mt-2">
+                    <button
+                      className="bg-green-500 rounded-md"
+                      onClick={() => handleEdit(file._id)}
+                    >
+                      edit
+                    </button>
+                    <button
+                      className="bg-red-500 rounded-md"
+                      onClick={() => handleDelete(file._id)}
+                    >
+                      delete
+                    </button>
+                  </div>
+                </div>
               </li>
             ))}
           </ul>
